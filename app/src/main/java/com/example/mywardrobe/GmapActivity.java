93d2078e3a8cc909463e99e8a6db1ba65fc439be
Andrew Oroh <andreworoh.ao@gmail.com>
2023-03-24 13:31:15 +0700

    Add Gmap Features
    point to binus alsut campus

--- app/src/main/java/com/example/mywardrobe/GmapActivity.java
+++ app/src/main/java/com/example/mywardrobe/GmapActivity.java
@@ -1,14 +1,38 @@
 package com.example.mywardrobe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class GmapActivity extends AppCompatActivity {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gmap);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.gMaps);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                map = googleMap;
+                LatLng campus = new LatLng(-6.2225, 106.6491);
+                map.addMarker(new MarkerOptions()
+                        .position(campus)
+                        .title("Wardrobe in campus"));
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(campus,15));
+            }
+        });
     }
 }
